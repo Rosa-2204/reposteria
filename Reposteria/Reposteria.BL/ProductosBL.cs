ef479614e5ba8adb228d67113534e9f5a8fdc008
Shelly <Shelly@DESKTOP-9HPSIB0>
2021-06-23 19:21:17 -0600

    Agregando arquitectura básica

---
+++ Reposteria/Reposteria.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reposteria.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pastel tres leches";
+            producto1.Precio = 300;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Cheesecake";
+            producto2.Precio = 250;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pastel helado de piña";
+            producto3.Precio = 350;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
--- Reposteria/Resposteria.Web/Controllers/ProductosController.cs
+++ Reposteria/Resposteria.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using Resposteria.Web.Models;
+﻿using Reposteria.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace Resposteria.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pastel tres leches";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Cheesecake";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pastel helado de piña";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
